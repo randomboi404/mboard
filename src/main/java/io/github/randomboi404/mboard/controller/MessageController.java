6913e691d890b1ced9445eca04cd3282bd181bc2
randomboi404 <tanishsawant603@gmail.com>
2026-03-23 08:41:05 +0530

    feat: add pagination for messages

--- src/main/java/io/github/randomboi404/mboard/controller/MessageController.java
+++ src/main/java/io/github/randomboi404/mboard/controller/MessageController.java
@@ -5,13 +5,14 @@ import io.github.randomboi404.mboard.service.MessageService;
 import io.github.randomboi404.mboard.dto.MessageRequest;
 import io.github.randomboi404.mboard.dto.TypingRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
+
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
@@ -20,53 +21,62 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
-    
+
     private final MessageService messageService;
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
-    
+
     @GetMapping("/api/v1/messages")
+    @Deprecated
     public List<Message> getMessages() {
         return messageService.getMessages();
     }
-    
+
+    @GetMapping("/api/v3/messages")
+    public Page<Message> getMessagePage(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        return messageService.getMessages(PageRequest.of(page, size, Sort.by("dateTime").descending()));
+    }
+
     @GetMapping(value = "/api/v2/messages/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter streamMessages() {
         SseEmitter emitter = new SseEmitter(300_000L);
-        
+
         this.emitters.add(emitter);
-        
+
         emitter.onCompletion(() -> this.emitters.remove(emitter));
         emitter.onTimeout(() -> this.emitters.remove(emitter));
         emitter.onError((e) -> this.emitters.remove(emitter));
 
         return emitter;
     }
-    
+
     @PostMapping("/api/v2/typing")
     public void typing(@RequestBody TypingRequest request) {
         SseEventBuilder builder = SseEmitter.event()
-            .name("typing")
-            .data(request);
-        
+                .name("typing")
+                .data(request);
+
         this.sendMessage(builder);
     }
-    
+
     @PostMapping("/api/v1/messages")
     public void saveMessage(@RequestBody MessageRequest request) {
         String username = request.username();
         String msgContent = request.message();
         String dateTime = Instant.now().toString();
-        
+
         Message message = new Message(username, msgContent, dateTime);
         messageService.saveMessage(message);
-        
+
         SseEventBuilder builder = SseEmitter.event()
-            .name("message")
-            .data(message);
-        
+                .name("message")
+                .data(message);
+
         this.sendMessage(builder);
     }
-    
+
     private void sendMessage(SseEventBuilder builder) {
         for (SseEmitter emitter : emitters) {
             try {
--- src/main/java/io/github/randomboi404/mboard/service/MessageService.java
+++ src/main/java/io/github/randomboi404/mboard/service/MessageService.java
@@ -3,6 +3,8 @@ package io.github.randomboi404.mboard.service;
 import io.github.randomboi404.mboard.model.Message;
 import io.github.randomboi404.mboard.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +15,13 @@ public class MessageService {
     
     private final MessageRepository msgRepo;
     
+    @Deprecated
     public List<Message> getMessages() {
         return msgRepo.findAll();
+    }
+
+    public Page<Message> getMessages(Pageable pageable) {
+        return msgRepo.findAll(pageable);
     }
     
     public void saveMessage(Message message) {
